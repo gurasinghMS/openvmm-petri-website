d6c7503eb6e787d8cdfbb6f655bfb1c482c8998f
Guramrit Singh <gurasingh@microsoft.com>
2025-09-29 18:35:03 -0700

    Build

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/openvmm-petri-website/dist">
       <QueryClientProvider client={queryClient}>
         {/* <Header /> */}
         <Content />
